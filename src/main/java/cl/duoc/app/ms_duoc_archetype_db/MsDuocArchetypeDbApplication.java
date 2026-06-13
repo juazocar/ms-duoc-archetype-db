a308d90d4555dee6ed589eb42e26049429d12fe5
Alumno Plaza Vespucio <pv-alumno@duoc.acad>
2026-06-13 08:54:19 -0400

    Primer commit de servicio db

---
+++ src/main/java/cl/duoc/app/ms_duoc_archetype_db/MsDuocArchetypeDbApplication.java
@@ -0,0 +1,13 @@
+package cl.duoc.app.ms_duoc_archetype_db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsDuocArchetypeDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsDuocArchetypeDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/duoc/app/ms_duoc_archetype_db/MsDuocArchetypeDbApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.duoc.app.ms_duoc_archetype_db;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsDuocArchetypeDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
